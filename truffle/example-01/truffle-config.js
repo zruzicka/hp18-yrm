5db678daf3b1923c11954f216396d427072bb306
Zbynek Ruzicka <zruzicka@email.cz>
2018-06-24 10:25:00 +0200

    (truffle) * Added test for Adoption contract. * Initial ropsten network.

--- truffle/example-01/truffle-config.js
+++ truffle/example-01/truffle-config.js
@@ -5,7 +5,15 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: 5777 // Match any network id
+      network_id: "*" //1 //5777 // Match any network id
     }
+    /*
+    ,
+    ropsten: {
+      network_id: 3,    // Official ropsten network id
+      provider: engine, // Use our custom provider
+      from: address,     // Use the address we derived
+      gas: 3000000
+    }*/
   }
 };
